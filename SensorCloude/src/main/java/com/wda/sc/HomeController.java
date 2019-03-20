74a2493eeb9b6828a4afccf671ac013eae20c616
Lsanta <38778788+Lsanta@users.noreply.github.com>
2019-03-20 20:22:48 +0900

    1
    1

--- SensorCloude/src/main/java/com/wda/sc/HomeController.java
+++ SensorCloude/src/main/java/com/wda/sc/HomeController.java
@@ -1,8 +1,11 @@
 package com.wda.sc;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
+
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +13,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.wda.sc.domain.memberVO;
+import com.wda.sc.service.LoginService;
+
 
 /**
  * Handles requests for the application home page.
@@ -17,23 +26,39 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private LoginService loginservice;
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		System.out.println("gd");
+		return "main";
 	}
 	
+	@RequestMapping("main")
+	public String main(Locale locale, Model model) {
+	
+		return "main";
+	}
+	
+	@RequestMapping("/login.do")
+	@ResponseBody
+	public String loginCheck(Model model,HttpSession session, @RequestParam String id,@RequestParam String password) {
+    
+		ArrayList<memberVO> arr = new ArrayList<memberVO>();
+		arr = loginservice.login(id);
+		
+		if(arr.size() != 0) {
+			
+			if(arr.get(0).getPassword().equals(password)) {
+				model.addAttribute("id",arr.get(0).getUserid());
+				session.setAttribute("id", arr.get(0).getUserid());
+				return "success";
+			}else {
+				return "fail";
+			}
+		}else {
+			return "fail";
+		}
+		
+	}
 }
