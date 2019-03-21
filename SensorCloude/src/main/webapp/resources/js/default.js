51656c172d085d0b0923360ac18301b87e611aca
An-Seong-Min <46548678+An-Seong-Min@users.noreply.github.com>
2019-03-21 12:31:07 +0900

    메인

---
+++ SensorCloude/src/main/webapp/resources/js/default.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    $(".all").on("click",function(){
+        $(".nav").addClass("on");
+        $(".dim").show();
+    });
+    $(".nav .close").on("click",function(){
+        $(".nav").removeClass("on");
+        $(".dim").hide();
+    });
+    $(".man").on("click",function(){
+        $(".a2").addClass("open");
+        $(".dim").show();
+    });
+    $(".manclose").on("click",function(){
+        $(".a2").removeClass("open");
+        $(".dim").hide();
+    });
+                  
+    $(".dim").on("click",function(){
+        $(".nav").removeClass("on");
+        $(".dim").hide();
+    });
+     $(".dim").on("click",function(){
+        $(".a2").removeClass("open");
+        $(".dim").hide();
+    });
+            
+});
