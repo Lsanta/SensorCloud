03e2d7101c6f822a7c34633ff61a77eedcbc0d72
An-Seong-Min <46548678+An-Seong-Min@users.noreply.github.com>
2019-03-25 18:55:36 +0900

    커밋
    ㅇㅇ

--- SensorCloude/src/main/java/com/wda/sc/SiteController.java
+++ SensorCloude/src/main/java/com/wda/sc/SiteController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.wda.sc.service.LoginService;
 import com.wda.sc.service.SiteService;
 
 import lombok.AllArgsConstructor;
