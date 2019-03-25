6bac04815d49f8adfac1c034fbdcea80bb7f65ef
yuuuuul <45421103+yuuuuul@users.noreply.github.com>
2019-03-25 18:54:14 +0900

    view

---
+++ SensorCloude/src/main/webapp/resources/js/check/checklist.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	$('.pagination-inner a').on('click', function() {
+		$(this).siblings().removeClass('pagination-active');
+		$(this).addClass('pagination-active');
+	});
+
+});
