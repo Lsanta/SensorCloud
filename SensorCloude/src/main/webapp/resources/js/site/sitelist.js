d628cbe6e8bbb769870fb40ca654657d33ba9734
An-Seong-Min <46548678+An-Seong-Min@users.noreply.github.com>
2019-03-25 19:44:35 +0900

    점검 이력 클릭시 현장 메인화면이동
    sid 라는 현장id를 통해서 이동

--- SensorCloude/src/main/webapp/resources/js/site/sitelist.js
+++ SensorCloude/src/main/webapp/resources/js/site/sitelist.js
@@ -2,20 +2,20 @@
  * 
  */
 
-$(document).ready(function () {
-	$('#add').click(function() {
-		window.location.href = "siteadd";
-	});
+/**
+ * 
+ */
 
-	$(document).on("click", "#site > tr" , function(){
-		/*현장 아이디를 가지고와서 /1자리에 넣어야함 */
-		window.location.href = "/site" + "/1"  ;
-
-	});
-
-
+$(document).ready(function(){
+		
+	 $(document).on("click", "#site > tr" , function(){
+		 /*현장 아이디를 가지고와서 /1자리에 넣어야함 */
+		 var sid = $("#sid").text();
+		 alert(sid);
+		 window.location.href = "site/" + sid  ;
+		 
+	 });
 });
-
 
 $(document).ready(function() {
 
