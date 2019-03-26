341657038ce34fab1a4b054e8abb886a83ddbe5d
Lsanta <38778788+Lsanta@users.noreply.github.com>
2019-03-26 18:59:20 +0900

    gg

--- SensorCloude/src/main/webapp/resources/js/main.js
+++ SensorCloude/src/main/webapp/resources/js/main.js
@@ -1,14 +1,12 @@
 /**
  * 
  */
-
 $(document).ready(function(){
 		
 	 $(document).on("click", "#site tr" , function(){
 		 /*현장 아이디를 가지고와서 /1자리에 넣어야함 */
 		 var tr = $("#site tr").index(this);
 		 var n = $("#site tr:eq("+tr+") td:eq(4)").text();
-		 alert(n);
 		 window.location.href = "/site/" + n  ;
 		 
 	 });
