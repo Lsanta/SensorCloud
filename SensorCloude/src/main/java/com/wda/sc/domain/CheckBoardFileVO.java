8f7dc0b241f107057991654a7d25051500b15d9a
Lsanta <38778788+Lsanta@users.noreply.github.com>
2019-03-28 09:52:06 +0900

    ddd

--- SensorCloude/src/main/java/com/wda/sc/domain/CheckBoardFileVO.java
+++ SensorCloude/src/main/java/com/wda/sc/domain/CheckBoardFileVO.java
@@ -3,7 +3,7 @@ package com.wda.sc.domain;
 import lombok.Data;
 
 @Data
-public class CheckBoard_fileVO {
+public class CheckBoardFileVO {
 	private String board_file_id;
 	private int board_no;
 	private String file_path;
