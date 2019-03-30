7e3ba76f59c0ab9cac93268398c30fe14ad627a8
kdn1391 <46514429+kdn1391@users.noreply.github.com>
2019-03-30 17:08:44 +0900

    1
    1

--- SensorCloude/src/main/java/com/wda/sc/CheckboardCotroller.java
+++ SensorCloude/src/main/java/com/wda/sc/CheckboardCotroller.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,9 +18,25 @@ import lombok.AllArgsConstructor;
 public class CheckboardCotroller {
 	private CheckboardService Checkboardservice;
 	
+	
 	@RequestMapping(value = "checkadd", method = RequestMethod.GET)
 	public String address(Locale locale, Model model) {
 	
 		return "check/checkadd";
 	}
+	
+	
+	@RequestMapping(value= "{board_no}", method = RequestMethod.GET)
+	public String checkin(Locale locale,@PathVariable  String board_no, Model model) {
+	
+		System.out.println("보드넘버=" +board_no);
+		
+		model.addAttribute("cklist",Checkboardservice.viewgetList(board_no));
+		
+		System.out.println(Checkboardservice.viewgetList(board_no));
+		
+		return "check/checkview";
+	}
+	
+
 }
--- SensorCloude/src/main/webapp/resources/js/check/checklist.js
+++ SensorCloude/src/main/webapp/resources/js/check/checklist.js
@@ -4,5 +4,23 @@ $(document).ready(function() {
 		$(this).siblings().removeClass('pagination-active');
 		$(this).addClass('pagination-active');
 	});
+	
+	
+	$(document).on("click", "#checklist tr" , function(){
+		
+		//클릭한 행을 tr 변수로 
+		 var tr = $("#checklist tr").index(this);
+		 
+		 alert(2222);
+		 
+		 var s =$("#checklist tr:eq("+tr+") td:eq(5) ").text();
+		 
+		 
+		 //내가클릭한 테이블의 행을 판별해야하기위해  board_no 정보를 넘긴다 
+		 window.location.href = "/checkboard/" + s ;
+		 
+	 });
+	
+	
 
 });
