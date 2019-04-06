0257808afcf2be93eae8a3b01ca2301ec390b9b7
yuuuuul <45421103+yuuuuul@users.noreply.github.com>
2019-04-06 16:22:10 +0900

    substring

--- SensorCloude/src/main/java/com/wda/sc/HomeController.java
+++ SensorCloude/src/main/java/com/wda/sc/HomeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.wda.sc.domain.CheckBoardVO;
 import com.wda.sc.domain.InstallSensorVO;
 import com.wda.sc.domain.MysensorVO;
 import com.wda.sc.domain.Paging;
@@ -48,9 +49,18 @@ public class HomeController {
 
 	@RequestMapping("main")
 	public String main(Locale locale, Model model) {
+		
+		//메인화면 점검이력 제목 substring
+		ArrayList<CheckBoardVO> arr = checkboardservice.mainList();
+	
+		for(int i = 0; i < arr.size(); i++) {
+			if(arr.get(i).getTitle().length() > 6) {
+				arr.get(i).setTitle(arr.get(i).getTitle().substring(0,6)+"...");
+			}
+		}
 		model.addAttribute("sitelist",siteservice.getList());
 		model.addAttribute("timelinelist",timelineservice.timedesc());
-		model.addAttribute("mainchecklist",checkboardservice.mainList());
+		model.addAttribute("mainchecklist",arr);
 		return "main";
 	}
 
@@ -60,7 +70,7 @@ public class HomeController {
 		int pageNum = 0;
 		ArrayList<Integer> arr = new ArrayList<Integer>();
 		int realNum = Integer.parseInt(num);
-		
+
 		page.setTotalNum(checkboardservice.getPageNum());
 
 		System.out.println(page.getTotalNum());
@@ -69,7 +79,7 @@ public class HomeController {
 		}else {
 			pageNum = page.getTotalNum()/page.getOnePageBoard();
 		}
-		
+
 		for(int i = 0; i < pageNum; i ++) {
 			arr.add(i+1);
 		}
@@ -89,7 +99,7 @@ public class HomeController {
 		ArrayList<Integer> arr = new ArrayList<Integer>();
 		int realNum = Integer.parseInt(num);
 		page.setTotalNum(siteservice.getPageNum());
-		
+
 		if(page.getTotalNum() < page.getOnePageBoard() ) {
 			pageNum = 1;
 		}else {
@@ -106,7 +116,7 @@ public class HomeController {
 		model.addAttribute("content",siteservice.getContent(page));
 		model.addAttribute("pageNum",arr);
 		model.addAttribute("sitelist",siteservice.getList());
-		
+
 		return "site/sitelist";
 	}
 
@@ -118,13 +128,13 @@ public class HomeController {
 
 	@RequestMapping(value = "/manage"+"/{num}", method = RequestMethod.GET)
 	public String manage(@PathVariable String num, Model model) {
-		
+
 		Paging page = new Paging();
 		int pageNum = 0;
 		ArrayList<Integer> arr = new ArrayList<Integer>();
 		int realNum = Integer.parseInt(num);
 		page.setTotalNum(usermanageservice.getPageNum());
-		
+
 		if(page.getTotalNum() < page.getOnePageBoard() ) {
 			pageNum = 1;
 		}else {
@@ -147,16 +157,16 @@ public class HomeController {
 	public String timeline(Locale locale, Model model) {
 		model.addAttribute("timelinelist",timelineservice.getList());
 		System.out.println(timelineservice.getList());
-		
+
 		return "timeline/timeline";
 	}
 
 	@RequestMapping(value = "/mysensor", method = RequestMethod.GET)
 	public String mysensor(Locale locale, Model model) {
-			
+
 		model.addAttribute("sensorlist",mysensorservice.getList());
-	
-		
+
+
 		return "mysensor/mysensor";
 	}
 
@@ -165,27 +175,27 @@ public class HomeController {
 
 		Object id = (Object)session.getAttribute("id");
 		int pageNum=0;
-		
+
 		Paging page = new Paging();
 		Map<String, Object> parm = new HashMap<String, Object>();
 		ArrayList<Integer> arr = new ArrayList<Integer>();
 		int realNum = Integer.parseInt(num);
-		
+
 		page.setTotalNum(mypageservice.getPageNum(id.toString()));
-		
+
 		if(page.getTotalNum() < page.getOnePageBoard() ) {
 			pageNum = 1;
 		}else {
 			pageNum = page.getTotalNum()/page.getOnePageBoard();
 		}
-		
+
 		for(int i = 0; i < pageNum; i ++) {
 			arr.add(i+1);
 		}
 
 		page.setEndnum((realNum*10)+1);
 		page.setStartnum(page.getEndnum()-10);
-		
+
 		parm.put("paging", page);
 		parm.put("user_id", id);
 
@@ -194,6 +204,6 @@ public class HomeController {
 		model.addAttribute("mychecklist",mypageservice.myList(parm));
 		return "mypage/mypage";
 	}
-	
+
 }
 
