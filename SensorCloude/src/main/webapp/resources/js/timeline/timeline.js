9c0205b75cdbf963d926c97508c36918b1c4b2be
s2k0j798 <45255056+s2k0j798@users.noreply.github.com>
2019-04-16 20:54:52 +0900

    a

--- SensorCloude/src/main/webapp/resources/js/timeline/timeline.js
+++ SensorCloude/src/main/webapp/resources/js/timeline/timeline.js
@@ -34,6 +34,64 @@ $(document).ready(function(){
 		});
 		
 	}); 
+	
+	$(".delete").on('click', function() {
+
+		if (confirm("정말 삭제하시겠습니까?") == true) {
+
+		} else {
+			return;
+		}
+
+		if (true) {
+			var content = $(this).parent();
+			var content2 = content.siblings().first();
+			var content3 = content2.text();
+
+			var query = {
+				content : content3
+			}
+
+			$.ajax({
+				type : "POST",
+				url : "/timeline/timelinedelete.do",
+				data : query,
+				success : function(data) {
+					if (data == "success") {
+						alert("삭제되었습니다");
+						location.reload();
+					} else {
+						alert("삭제에 실패했습니다");
+					}
+				}
+
+			}); // ajax 종료
+			/*}*/
+		}
+
+	});
+
+	$(".modify").on('click',function() {
+
+				var div = $(this).parent().attr('id');
+							
+				var content = $(this).parent();
+				var content2 = content.siblings().first();
+				var content3 = content2.text();
+
+				var query = {
+					content : content3
+				}
+
+				window.location.href = "timeline/timelinemodify?content="+ content3+"/"+div;
+
+			});
+	$('#textarea').keypress(function(event){
+	     if ( event.which == 13 ) {
+	         $('#submit').click();
+	     
+	     }
+	});
 	var num = 0;
 
 	var newURL =  window.location.pathname;
