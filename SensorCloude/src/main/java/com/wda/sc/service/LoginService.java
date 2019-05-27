7793893b4e45d4981828b3f43eda152389865791
yuuuuul <45421103+yuuuuul@users.noreply.github.com>
2019-05-27 14:39:52 +0900

    이메일 인증

--- SensorCloude/src/main/java/com/wda/sc/service/LoginService.java
+++ SensorCloude/src/main/java/com/wda/sc/service/LoginService.java
@@ -1,6 +1,7 @@
 package com.wda.sc.service;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import com.wda.sc.domain.MemberVO;
 
@@ -9,4 +10,5 @@ public interface LoginService {
 	public ArrayList<MemberVO> login(String id);
 	public int signup(MemberVO member);
 	public ArrayList<MemberVO> idFind(String name);
+	public int keyUpdate(Map<String, String> map);
 }
