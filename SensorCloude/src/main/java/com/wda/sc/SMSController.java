3cd82a3e290179b636d57036433accb4299a4cbc
An-Seong-Min <46548678+An-Seong-Min@users.noreply.github.com>
2019-06-04 11:36:17 +0900

    sms 기능 추가

--- SensorCloude/src/main/java/com/wda/sc/SMSController.java
+++ SensorCloude/src/main/java/com/wda/sc/SMSController.java
@@ -1,123 +1,91 @@
 package com.wda.sc;
 
-import java.io.*;
-import java.net.*;
-import java.security.*;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Locale;
-import javax.net.ssl.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import lombok.AllArgsConstructor;
+import com.wda.sc.domain.AlarmVO;
+import com.wda.sc.service.CheckboardService;
+import com.wda.sc.service.SiteService;
 
+import lombok.AllArgsConstructor;
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+
+/**
+ * @class ExampleSend
+ * @brief This sample code demonstrate how to send sms through CoolSMS Rest API PHP
+ */
 @Controller
 @AllArgsConstructor
 @RequestMapping("/SMS")
 public class SMSController {
+	private SiteService siteservice;
 	
-	@SuppressWarnings("restriction")
-	@RequestMapping(value = "/SMSTest", method = RequestMethod.GET)
-	public String SMSMS(Locale locale, Model model) {
-		System.out.println("문자 전송 컨트롤러");
+	//알람 전송을 누르면 
+	@RequestMapping(value = "/alarmadd.do")
+	@ResponseBody
+	public String alarmadd(AlarmVO vo, HttpSession session) {
+		System.out.println("문자전송 컨트롤러");
 		
-		/*
-		 * 뿌리오 발송API 경로 - 서버측 인코딩과 응답형태에 따라 선택
-		 */
-		//String api_url = "https://www.ppurio.com/api/send_euckr_text.php";    // EUC-KR 인코딩과 TEXT 응답용 호출 페이지
-		// String api_url = "https://www.ppurio.com/api/send_euckr_json.php"; // EUC-KR 인코딩과 JSON 응답용 호출 페이지
-		// String api_url = "https://www.ppurio.com/api/send_euckr_xml.php";  // EUC-KR 인코딩과 XML 응답용 호출 페이지
-		 String api_url = "https://www.ppurio.com/api/send_utf8_text.php";  // UTF-8 인코딩과 TEXT 응답용 호출 페이지
-		// String api_url = "https://www.ppurio.com/api/send_utf8_json.php";  // UTF-8 인코딩과 JSON 응답용 호출 페이지
-		// String api_url = "https://www.ppurio.com/api/send_utf8_xml.php";   // UTF-8 인코딩과 XML 응답용 호출 페이지
+		String content = vo.getAlarm_content();
+		// 연락망 추가 폼을 이용한 추가
+		String user = (String) session.getAttribute("id");
+		vo.setSend_user(user);
+		
+		int a = siteservice.insertAlarm(vo);
 
+		
+		if (a == 0) {
+			return "false";
+		} else if (a == 1) {
+			
+		    String api_key = "NCSUHVPGXGVJOVA2";
+		    String api_secret = "FCIGN0PTFU97R3KHOOD7HR6WUDRF6PPU";
+		    Message coolsms = new Message(api_key, api_secret);
+		    
+		    
+		    
+		    // 4 params(to, from, type, text) are mandatory. must be filled
+		    HashMap<String, String> params = new HashMap<String, String>();
+		    params.put("to", "01051331449,01027901543");
+		    params.put("from", "01046037101");
+		    params.put("type", "SMS");
+		    params.put("text", "테스트입니다.");
+		    params.put("app_version", "test app 1.2"); // application name and version
 
-		/*
-		 * 요청값
-		 */
-		String userid = "gkgkrk123";           // [필수] 뿌리오 아이디
-		String callback = "01046037101";    // [필수] 발신번호 - 숫자만
-		String phone = "01051331449";       // [필수] 수신번호 - 여러명일 경우 |로 구분 "010********|010********|010********"
-		String msg = "테스트 발송입니다";   // [필수] 문자내용 - 이름(names)값이 있다면 [*이름*]가 치환되서 발송됨
-		String names = "홍길동";            // [선택] 이름 - 여러명일 경우 |로 구분 "홍길동|이순신|김철수"
-		String appdate = "";  // [선택] 예약발송 (현재시간 기준 10분이후 예약가능)
-		String subject = "테스트";          // [선택] 제목 (30byte)
-
-
-		try {
-
-			String str = "userid="+userid+"&callback="+callback+"&phone="+phone+"&msg="+msg+"&names="+names+"&appdate="+appdate+"&subject="+subject;
-		    System.out.println(str);
-		    URL url = new URL(api_url);
-		    System.out.println(api_url);
-		    Security.addProvider(
-		      new com.sun.net.ssl.internal.ssl.Provider());
-		    System.out.println("여기는 오나?00");
-		    SSLSocketFactory factory = 
-		      (SSLSocketFactory)SSLSocketFactory.getDefault();
-		    System.out.println("여기는 오나?0.52");
-		    SSLSocket socket = 
-		      (SSLSocket)factory.createSocket(url.getHost(), 8080);
-		    System.out.println("여기는 오나?11");
-		    PrintWriter out = new PrintWriter(
-		        new OutputStreamWriter(
-		            socket.getOutputStream()));
-		    System.out.println("여기는 오나?22");
-		    out.println("POST "+ api_url +" HTTP/1.1");
-		    out.println("Content-type: application/x-www-form-urlencoded");
-		    out.println("Content-length: "+ str.getBytes().length +"\r\n");
-		    out.println(str);
-		    out.flush();
-
-		    BufferedReader in = new BufferedReader(
-		        new InputStreamReader(
-		        socket.getInputStream()));
-
-		    String line;
-
-		    while ((line = in.readLine()) != null) {
-		        System.out.println(line);
+		    try {
+		      JSONObject obj = (JSONObject) coolsms.send(params);
+		      System.out.println(obj.toString());
+		    } catch (CoolsmsException e) {
+		      System.out.println(e.getMessage());
+		      System.out.println(e.getCode());
 		    }
-
-		    out.close();
-		    in.close();
-
-		} catch (IOException localIOException) {
-		    // out.println(localIOException.toString());
-		    System.out.println("에러" + localIOException.toString());
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			return "success";
 		}
 
-
-		/*
-		 * 응답값
-		 *
-		 *  <성공시>
-		 *    "ok|sms|123456789|1"           - 전송결과|발송형태(단문은 sms 장문은 lms 포토문자는 mms)|발송 msgid (예약취소시 필요)|발송건수
-		 *
-		 *  <실패시>
-		 *    "invalid_member"               - 연동서비스 신청이 안 됐거나 없는 아이디
-		 *    "under_maintenance"            - 요청시간에 서버점검중인 경우
-		 *    "allow_https_only"             - http 요청인 경우
-		 *    "invalid_ip"                   - 등록된 접속가능 IP가 아닌 경우
-		 *    "invalid_msg"                  - 문자내용에 오류가 있는 경우
-		 *    "invalid_names"                - 이름에 오류가 있는 경우
-		 *    "invalid_subject"              - 제목에 오류가 있는 경우
-		 *    "invalid_sendtime"             - 예약발송 시간에 오류가 있는 경우 (10분이후부터 다음해말까지 가능)
-		 *    "invalid_sendtime_maintenance" - 예약발송 시간에 서버점검 예정인 경우
-		 *    "invalid_phone"                - 수신번호에 오류가 있는 경우
-		 *    "invalid_msg_over_max"         - 문자내용이 너무 긴 경우
-		 *    "invalid_callback"             - 발신번호에 오류가 있는 경우
-		 *    "once_limit_over"              - 1회 최대 발송건수 초과한 경우
-		 *    "daily_limit_over"             - 1일 최대 발송건수 초과한 경우
-		 *    "not_enough_point"             - 잔액이 부족한 경우
-		 *    "over_use_limit"               - 한달 사용금액을 초과한 경우
-		 *    "server_error"                 - 기타 서버 오류
-		 */
-
-		
-		return "site/sitealarm";
+		return "false";
 	}
 }
